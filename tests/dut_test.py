4504cc2c62a0d406d913e4405bb1f0b37314d10e
Vijayvithal Jahagirdar <jvs@dyumnin.com>
2025-04-02 09:50:22 +0530

    code

--- tests/dut_test.py
+++ tests/dut_test.py
@@ -4,4 +4,12 @@ from cocotb.triggers import Timer
 
 @cocotb.test()
 async def dut_test(dut):
-    assert 0, "Test Not Implemented Error"
+    a_in=[0,0,1,1]
+    b_in=[0,1,0,1]
+    y_out=[0,1,1,0]
+    for i in range(4):
+        dut.a.value =a_in[i]
+        dut.b.value=b_in[i]
+        await Timer(1,"ns")
+        assert dut.y.value == y_out[i]
+
